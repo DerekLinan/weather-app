9244d90de75b2116cbeb566b600affd5b79eaa60
DerekLinan <4261674+DerekLinan@users.noreply.github.com>
2022-02-23 15:18:22 -0700

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>Weather App</h1>
+  <div class="content">
+    <SearchLocationBar />
+    <CurrentWeather />
+    <hr />
+  </div>
+  <AppFooter />
+</template>
+
+<script>
+import CurrentWeather from "./components/CurrentWeather.vue";
+import SearchLocationBar from "./components/SearchLocationBar.vue";
+import AppFooter from "./components/AppFooter.vue";
+export default {
+  name: "App",
+  components: {
+    SearchLocationBar,
+    CurrentWeather,
+    AppFooter,
+  },
+};
+</script>
+
+<style>
+* {
+  border: none;
+  margin: 0;
+  padding: 0;
+}
+hr {
+  height: 1px;
+  background-color: white;
+  margin: 1rem;
+}
+.content {
+  flex-grow: 1;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  background: rgb(0, 255, 222);
+  background: linear-gradient(
+    5deg,
+    rgba(0, 255, 222, 1) 0%,
+    rgba(0, 237, 255, 1) 30%,
+    rgba(167, 119, 255, 1) 100%
+  );
+}
+</style>
---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Colorado Springs</h2>
+  <h3>Wed, February 23 1:45 PM</h3>
+  <div>
+    <span></span>
+    <p>7°</p>
+    <h2>Cloudy</h2>
+  </div>
+</template>
